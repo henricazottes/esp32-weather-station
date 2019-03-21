e0d2d3f65ac0b3bb16aac59f5176f97ff1ac8187
Henri Cazottes <henri.cazottes@gmail.com>
2019-03-21 01:52:06 +0100

    Refacto connexion error handling + clean code

--- main.py
+++ main.py
@@ -6,33 +6,35 @@ import temperature
 import utime
 import gc
 import micropython
+import wifimgr
 from machine import Pin, SPI, TouchPad, Timer
 from microWebCli import MicroWebCli
-import wifimgr
 
-touchMiddle = TouchPad(Pin(13))
-touchLeft   = TouchPad(Pin(0))
-touchRight  = TouchPad(Pin(2))
+## ==============================
+#  API Setup
+## ==============================
 
-touchMiddle.config(500)
-touchLeft.config(500)
-touchRight.config(500)
+location    = "Toulouse,fr"
+app_id      = "5eaadaddb1142fa0de056db0f11e647a"
+base_url    = "https://owm-proxy.herokuapp.com/"
 
-touchPads       = [touchLeft, touchMiddle, touchRight]
-touchStates     = [0,0,0]
-tim = Timer(-1)
+weather_url     = base_url + "weather" + "?q=" + location + "&appid=" + app_id
+forecast_url    = base_url + "forecast" + "?q=" + location + "&appid=" + app_id
+
 
 # GLOBALS
 
-TRESHOLD    = 150
-timestamp_g = 999999999
-wlan        = None
+TRESHOLD            = 150
+TIME_OFFSET         = 60*60     # French time offset
+TIME_DIFF           = 946684800 # Time diff between Micropython ref (2000) and Api ref (1976)
+wlan                = None
+timestamp_g         = 999999999
+weather_g           = {}
+week_icon_ids_g    = []
 
 
-# Time diff between Micropython ref (2000) and Api ref (1976)
-time_diff = 946684800
+# LCD Setup
 
-# Lcd Setup
 spi = SPI(1, baudrate=328125, bits=8, polarity=0, phase=1, sck=Pin(18), mosi=Pin(23), miso=Pin(19))
 spi.init()
 cs = Pin(5, Pin.OUT)
@@ -44,13 +46,17 @@ lcd.reset()
 lcd.init()
 lcd.clear()
 
+
 # Font setup
+
 font = font5x8.Font5x8(lcd, space=1)
 tempFont = temperature.Temperature(lcd)
 pictoBig = picto_big.picto_big(lcd)
 pictoSmall = picto_small.picto_small(lcd)
 
+
 # Weather id categories
+
 categories_ids = [
     [800],                                              #very_sunny_ids
     [801],                                              #sunny_ids 
@@ -63,15 +69,20 @@ categories_ids = [
     [600,601,602,611,612,615,616,620,621,622]           #snow_ids 
 ]
 
-# API Setup
-location = "?q=Toulouse,fr"
-app_id = "" # "&appid=5eaadaddb1142fa0de056db0f11e647a" Set your app id here
-base_url = "https://owm-proxy.herokuapp.com/"
-weather_url = base_url + "weather" + location + app_id
-forecast_url = base_url + "forecast" + location + app_id
 
-def switchOffBl():
-    bl.off()
+# Touch Pad configuration
+
+touchMiddle = TouchPad(Pin(13))
+touchLeft   = TouchPad(Pin(0))
+touchRight  = TouchPad(Pin(2))
+
+touchMiddle.config(500)
+touchLeft.config(500)
+touchRight.config(500)
+
+touchPads       = [touchLeft, touchMiddle, touchRight]
+touchStates     = [0,0,0]
+tim = Timer(-1)
 
 def leftHandler():
     print("Left touched")
@@ -86,24 +97,30 @@ def middleHandler():
 
 touchHandlers   = [leftHandler, middleHandler, rightHandler]
 
+
+# Functions definition
+
 def getWeekDay(timestamp):
-    return utime.localtime(timestamp - time_diff)[6]
+    return utime.localtime(timestamp - TIME_DIFF)[6]
+
 
 def getDate(timestamp):
-    localtime = utime.localtime(timestamp - time_diff)
+    localtime = utime.localtime(timestamp - TIME_DIFF)
     month   = str(localtime[1])
     day     = str(localtime[2])
     month   = month if len(month) > 1 else ("0" + month)
     day     = day if len(day) > 1 else ("0" + day)
     return day + "/" + month
 
+
 def getTime(timestamp):
-    localtime = utime.localtime(timestamp - time_diff)
+    localtime = utime.localtime(timestamp - TIME_DIFF)
     hours   = str(localtime[3])
     mins    = str(localtime[4])
     mins    = mins if len(mins) > 1 else ("0" + mins)
     hours   = hours if len(hours) > 1 else ("0" + hours)
     return hours + ":" + mins
+
 
 def getIconId(weather_id):
     icon_id = -1
@@ -122,6 +139,7 @@ def getWeekiconIds():
         week_icon_ids[index] = getIconId(weather_id)
     return week_icon_ids
 
+
 def getWeather():
     print("Weather url: " + weather_url)
     weather = {}
@@ -130,6 +148,7 @@ def getWeather():
     weather["temp"]         = int(round(result["temp"]))
     weather["dt"]           = result['dt']
     return weather
+
 
 def drawWeather(weather):
     lcd.position(0,3)
@@ -141,14 +160,15 @@ def drawWeather(weather):
     tempFont.print(str(weather['temp'])+"'C")
     lcd.relativeCursorMove(10,0)
     pictoBig.print(str(weather['weather_id']))
-    lcd.position(20,5)
-    # font.print(str(weather['min_temp'])+ " / " + str(weather['max_temp']))
-    
+
+
+
 def drawWeekDays(weekday):
     weekdayInitials = ["L", "M", "M", "J", "V", "S", "D"]
     for index, initial in enumerate(weekdayInitials):
         font.print(initial, 12*index + 5,0)
-    
+
+
 def drawWeekIcons(week_icon_ids, day_index):
     for index, icon_id in enumerate(week_icon_ids):
         if icon_id >= 0:
@@ -156,6 +176,7 @@ def drawWeekIcons(week_icon_ids, day_index):
         else:
             font.print("-", 12*index + 5, 1)
     font.print("^", 12*day_index + 5, 2)
+
 
 def drawDateAndTime(timestamp):
     date = getDate(timestamp)
@@ -165,20 +186,42 @@ def drawDateAndTime(timestamp):
     font.print(date, 5, 5)
     font.print(time, 55, 5)
 
-def run():
-    weather         = getWeather()
-    day_index       = getWeekDay(weather['dt'])
-    week_icon_ids   = getWeekiconIds()
-    timestamp_g     = weather['dt'] + 60*60 # French time offset
+
+def fetchData():
+    global weather_g
+    global week_icon_ids_g
+    global timestamp_g
+    global wlan
+    fail_counter    = 0
+    success         = False
+
+    while (fail_counter < 3) and (not success):
+        try:
+            weather_g       = getWeather()
+            week_icon_ids_g = getWeekiconIds()
+            success         = True
+        except:
+            fail_counter   += 1
+    if success:
+        timestamp_g = weather_g['dt'] + TIME_OFFSET
+        return
+    # Will force reconnection to wifi
+    wlan.disconnect()
+    wlan = None
+
+
+def redraw():
+    global timestamp_g
+    global weather_g
+    global week_icon_ids_g
 
     lcd.clear()
-    
+    day_index = getWeekDay(timestamp_g)
     drawWeekDays(day_index)
-    drawWeekIcons(week_icon_ids, day_index)
+    drawWeekIcons(week_icon_ids_g, day_index)
     drawDateAndTime(timestamp_g)
-    drawWeather(weather)
+    drawWeather(weather_g)
 
-    return timestamp_g
 
 def displayInstructions(ssid, pwd, url):
     print("Could not initialize the network connection.")
@@ -188,12 +231,15 @@ def displayInstructions(ssid, pwd, url):
     font.print("\n2. PWD:\n" + pwd)
     font.print("\n3. Open:\n" + url)
 
+
 def displaySuccess():
     lcd.clear()
     font.print("\n\nConnection Ok!")
     font.print("Weather check.", 0, 3)
 
-def verifyWifiStatus(wlan):
+
+def verifyWifiStatus():
+    global wlan
     if not wlan or (not wlan.isconnected()):
         lcd.clear()
         pictoSmall.print("0", 2, 0)
@@ -207,11 +253,12 @@ def verifyWifiStatus(wlan):
         utime.sleep(2)
         print("Done.")
 
+
 def checkTouchPads():
     for i in range(len(touchHandlers)):
         value = TRESHOLD
         # if i == 0:
-            # value = touchLeft.read()
+            # value = touchLeft.read()      # Not working for now, need debug
         if i == 1:
             value = touchMiddle.read()
         else:
@@ -225,12 +272,22 @@ def checkTouchPads():
             else:
                 rightHandler()
 
+
+## ==========================
+#  Main loop
+## ==========================
+
 while True:
-    verifyWifiStatus(wlan)
-    timestamp_g = run()
-    for i in range(10):
-        for j in range(240):
-            checkTouchPads()
-            utime.sleep(0.25)
-        timestamp_g += 60
-        drawDateAndTime(timestamp_g)
+    verifyWifiStatus()
+    fetchData()
+    if wlan is not None:
+        for i in range(10):
+            redraw()
+            # Next for loop is 1 min long
+            for j in range(240):
+                checkTouchPads()
+                utime.sleep(0.25)
+            timestamp_g += 60
+            if not wlan.isconnected():
+                wlan.disconnect()
+                break
